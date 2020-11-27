f39567f65b3bc3ed4d9dc7df49f089c5e96fe0eb
ashish336b <ashish336b@gmail.com>
2020-11-27 17:40:49 +0545

    scraped 8 data per site

--- server/app.js
+++ server/app.js
@@ -18,13 +18,14 @@ app.use(function (req, res, next) {
 
 app.get("/", async (req, res) => {
   try {
+    let noOfData = 8;
     const cnnNews = await cnnSpider(req.query.q);
     const bbcNews = await bbcSpider(req.query.q);
     const expressNews = await express_spider(req.query.q);
     res.json({
-      cnnNews: cnnNews.slice(0, 4),
-      bbcNews: bbcNews.slice(0, 4),
-      expressNews: expressNews.slice(0, 4),
+      cnnNews: cnnNews.slice(0, noOfData),
+      bbcNews: bbcNews.slice(0, noOfData),
+      expressNews: expressNews.slice(0, noOfData),
     });
   } catch (error) {
     console.log(error);
